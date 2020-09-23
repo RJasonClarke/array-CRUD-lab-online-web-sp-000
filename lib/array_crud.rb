69f9d8d9c2495f03b8cfd3501aaf0c310fa104a2
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-23 00:49:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array.pop
-  array = ["arrays!", "wow", "I", "am", "really", "learning"]
+  array = ["arrays!"]
 end
 
 def remove_element_from_start_of_array(array)
