d73cb3d1a822b2b97e6b07e9d3f32c56dc689c8c
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-23 00:40:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,21 +16,21 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = [1,2,3,4]
+
 end
 
 def remove_element_from_start_of_array(array)
-  array = [1,2,3,4]
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = [1,2,3,4]
+
 end
 
 def retrieve_first_element_from_array(array)
-  array = [1,2,3,4]
+
 end
 
 def retrieve_last_element_from_array(array)
-  array = [1,2,3,4]
+
 end
